cb27fca1fe92448fef1d91abdc1f544e83355f28
Ravi9550 <107767414+Ravi9550@users.noreply.github.com>
2024-09-07 04:45:15 +0530

    Update settings.py

--- student_management/settings.py
+++ student_management/settings.py
@@ -120,11 +120,13 @@ DATE_FORMAT = 'd-m-Y'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
+
 STATICFILES_DIRS = [
-    BASE_DIR / "static"
+    os.path.join(BASE_DIR,'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 
 
